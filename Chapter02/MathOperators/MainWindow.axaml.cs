8f421b2f033ab52ca047fd5c58d40c82d5319b5d
kriosereb <kriosereb@gmail.com>
2021-04-06 21:35:34 +0300

    Fix MathOperators

--- Chapter02/MathOperators/MainWindow.axaml.cs
+++ Chapter02/MathOperators/MainWindow.axaml.cs
@@ -5,80 +5,68 @@ using Avalonia.Markup.Xaml;
 using Avalonia.Interactivity;
 
 namespace MathOperators {
-	public class MainWindow : Window {
+	public partial class MainWindow : Window {
 		public MainWindow() {
 			InitializeComponent();
-#if DEBUG
-			this.AttachDevTools();
-#endif
-		}
-
-		private void InitializeComponent() {
-			AvaloniaXamlLoader.Load(this);
 		}
 
 		private void calculateClick(object sender, RoutedEventArgs e) {
 			try {
-				if ((bool)this.FindControl<RadioButton>("additional").IsChecked) {
+				if ((bool)addition.IsChecked) {
 					addValues();
-				} else if ((bool)this.FindControl<RadioButton>("subtraction").IsChecked) {
+				} else if ((bool)subtraction.IsChecked) {
 					substractValues();
-				} else if ((bool)this.FindControl<RadioButton>("multiplication").IsChecked) {
+				} else if ((bool)multiplication.IsChecked) {
 					multiplyValues();
-				} else if ((bool)this.FindControl<RadioButton>("division").IsChecked) {
+				} else if ((bool)division.IsChecked) {
 					divideValues();
-				} else if ((bool)this.FindControl<RadioButton>("remainder").IsChecked) {
+				} else if ((bool)remainder.IsChecked) {
 					remainderValues();
 				}
-			} catch (Exception caught){
-				 this.FindControl<TextBlock>("expression").Text = "";
-				this.FindControl<TextBlock>("result").Text = caught.Message;
+			} catch (Exception caught) {
+				experession.Text = "";
+				result.Text = caught.Message;
 			}
 		}
 
 		private void addValues() {
-			int lhs = int.Parse(this.FindControl<TextBox>("lhsOperand").Text);
-			int rhs = int.Parse(this.FindControl<TextBox>("rhsOperand").Text);
-			int outcome = 0;
-			// TODO: 
-			this.FindControl<TextBlock>("expression").Text = $"{lhs} + {rhs}";
-			this.FindControl<TextBlock>("result").Text = outcome.ToString();
+			int lhs = int.Parse(lhsOperand.Text);
+			int rhs = int.Parse(rhsOperand.Text);
+			int outcome = lhs + rhs;
+			experession.Text = $"{lhs} + {rhs}";
+			result.Text = outcome.ToString();
 		}
 
 		private void substractValues() {
-			int lhs = int.Parse(this.FindControl<TextBox>("lhsOperand").Text);
-			int rhs = int.Parse(this.FindControl<TextBox>("rhsOperand").Text);
-			int outcome = 0;
-			// TODO: 
-			this.FindControl<TextBlock>("expression").Text = $"{lhs} - {rhs}";
-			this.FindControl<TextBlock>("result").Text = outcome.ToString();
+			int lhs = int.Parse(lhsOperand.Text);
+			int rhs = int.Parse(rhsOperand.Text);
+			int outcome = lhs - rhs;
+			experession.Text = $"{lhs} - {rhs}";
+			result.Text = outcome.ToString();
 		}
 
 		private void multiplyValues() {
-			int lhs = int.Parse(this.FindControl<TextBox>("lhsOperand").Text);
-			int rhs = int.Parse(this.FindControl<TextBox>("rhsOperand").Text);
-			int outcome = 0;
-			// TODO: 
-			this.FindControl<TextBlock>("expression").Text = $"{lhs} * {rhs}";
-			this.FindControl<TextBlock>("result").Text = outcome.ToString();
+			int lhs = int.Parse(lhsOperand.Text);
+			int rhs = int.Parse(rhsOperand.Text);
+			int outcome = lhs * rhs;
+			experession.Text = $"{lhs} * {rhs}";
+			result.Text = outcome.ToString();
 		}
 
 		private void divideValues() {
-			int lhs = int.Parse(this.FindControl<TextBox>("lhsOperand").Text);
-			int rhs = int.Parse(this.FindControl<TextBox>("rhsOperand").Text);
-			int outcome = 0;
-			// TODO: 
-			this.FindControl<TextBlock>("expression").Text = $"{lhs} / {rhs}";
-			this.FindControl<TextBlock>("result").Text = outcome.ToString();
+			int lhs = int.Parse(lhsOperand.Text);
+			int rhs = int.Parse(rhsOperand.Text);
+			int outcome = lhs / rhs;
+			experession.Text = $"{lhs} / {rhs}";
+			result.Text = outcome.ToString();
 		}
 
 		private void remainderValues() {
-			int lhs = int.Parse(this.FindControl<TextBox>("lhsOperand").Text);
-			int rhs = int.Parse(this.FindControl<TextBox>("rhsOperand").Text);
-			int outcome = 0;
-			// TODO: 
-			this.FindControl<TextBlock>("expression").Text = $"{lhs} % {rhs}";
-			this.FindControl<TextBlock>("result").Text = outcome.ToString();
+			int lhs = int.Parse(lhsOperand.Text);
+			int rhs = int.Parse(rhsOperand.Text);
+			int outcome = lhs % rhs;
+			experession.Text = $"{lhs} % {rhs}";
+			result.Text = outcome.ToString();
 		}
 	}
 }
