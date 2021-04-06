07f0449f52323773e4a388d9ab80d57dafe2fbc7
kriosereb <kriosereb@gmail.com>
2021-04-06 21:07:55 +0300

    fix InitializeComponet

--- Chapter02/PrimitiveDataTypes/MainWindow.axaml.cs
+++ Chapter02/PrimitiveDataTypes/MainWindow.axaml.cs
@@ -3,21 +3,13 @@ using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 
 namespace PrimitiveDataTypes {
-	public class MainWindow : Window {
+	public partial class MainWindow : Window {
 		public MainWindow() {
 			InitializeComponent();
-#if DEBUG
-			this.AttachDevTools();
-#endif
-		}
-
-		private void InitializeComponent() {
-			AvaloniaXamlLoader.Load(this);
 		}
 
 		private void typeSelectedChanged(object sender, SelectionChangedEventArgs e) {
-			ListBox listBox = this.FindControl<ListBox>("type");
-			ListBoxItem selectedType = (ListBoxItem)listBox.SelectedItem;
+			ListBoxItem selectedType = (types.SelectedItem as ListBoxItem);
 			switch (selectedType.Content.ToString()) {
 				case "int":
 					showIntValue();
@@ -47,49 +39,41 @@ namespace PrimitiveDataTypes {
 		}
 
 		private void showIntValue() {
-			var value = this.FindControl<TextBox>("value");
 			int intVar = 42;
 			value.Text = intVar.ToString();
 		}
 
 		private void showLongValue() {
-			var value = this.FindControl<TextBox>("value");
 			long longVar = 42L;
 			value.Text = longVar.ToString();
 		}
 
 		private void showFloatValue() {
-			var value = this.FindControl<TextBox>("value");
 			float floatVar = 0.42F;
 			value.Text = floatVar.ToString();
 		}
 
 		private void showDoubleValue() {
-			var value = this.FindControl<TextBox>("value");
 			double doubleVar = 0.42;
 			value.Text = doubleVar.ToString();
 		}
 
 		private void showDecimalValue() {
-			var value = this.FindControl<TextBox>("value");
 			decimal decimalVar = 0.42M;
 			value.Text = decimalVar.ToString();
 		}
 
 		private void showStringValue() {
-			var value = this.FindControl<TextBox>("value");
 			string stringVar = "forty two";
 			value.Text = stringVar;
 		}
 
 		private void showCharValue() {
-			var value = this.FindControl<TextBox>("value");
 			char charVar = 'x';
 			value.Text = charVar.ToString();
 		}
 
 		private void showBoolValue() {
-			var value = this.FindControl<TextBox>("value");
 			bool boolVar = false;
 			value.Text = boolVar.ToString();
 		}
