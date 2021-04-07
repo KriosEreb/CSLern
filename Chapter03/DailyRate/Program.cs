086901d70cc296015e5a463c88d768d5616cbcb9
kriosereb <kriosereb@gmail.com>
2021-04-07 17:08:41 +0300

    Add DailyRate in Chapter03

---
+++ Chapter03/DailyRate/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DailyRate {
+	class Program {
+		static void Main(string[] args) {
+			(new Program()).run();
+		}
+
+		void run() {
+			double dailyRate = readDouble("Enter your daily rate: ");
+			int noOfDays = readInt("Enter the number of days: ");
+			writeFree(calculateFree(dailyRate, noOfDays));
+		}
+
+		private double readDouble(string v) {
+			Console.Write(v);
+			string line = Console.ReadLine();
+			return double.Parse(line);
+		}
+
+		private int readInt(string v) {
+			Console.Write(v);
+			string line = Console.ReadLine();
+			return int.Parse(line);
+		}
+
+		private double calculateFree(double dailyRate, int noOfDays) => dailyRate * noOfDays;
+
+		private void writeFree(double v) => Console.WriteLine($"The consultant's fee is: {v * 1.1}");
+	}
+}
