ddc7afa7ac099b31b825891977cabf448f1173d0
kriosereb <kriosereb@gmail.com>
2021-04-10 16:52:30 +0300

    Add DailyRate in Chapter03

---
+++ Chapter03/DailyRateUOP/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DailyRateUOP {
+	class Program {
+		static void Main(string[] args) {
+			(new Program()).run();
+		}
+
+		void run() {
+			double fee = calculateFree(theDailyRate: 375.0);
+			Console.WriteLine($"Fee is {fee}");
+		}
+
+		private double calculateFree(double theDailyRate = 500.0, int noOfDays = 1) {
+			Console.WriteLine("calculateFree using optional parameters");
+			return theDailyRate * noOfDays;
+		}
+
+		private double calculateFree(double dailyRate = 500.0) {
+			Console.WriteLine("calculateFree using one parametr");
+
+			int defaultNoOfDays = 1;
+			return dailyRate * defaultNoOfDays;
+		}
+
+		private double calculateFree() {
+			Console.WriteLine("calculateFree using hardcoded values");
+
+			double defaultDailyRate = 400.0;
+			int defaultNoOfDays = 1;
+			return defaultDailyRate * defaultNoOfDays;
+		}
+	}
+}
