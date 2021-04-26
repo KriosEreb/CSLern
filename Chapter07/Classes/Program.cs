bdd45c9a7d54baa7615f4d484ca4fa285d5f3103
kriosereb <kriosereb@gmail.com>
2021-04-26 16:44:47 +0300

    Add Classes in  Chapter07

---
+++ Chapter07/Classes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Classes {
+	class Program {
+		static void Main(string[] args) {
+			try {
+				doWork();
+			} catch (Exception ex) {
+				Console.WriteLine(ex.Message);
+			}
+		}
+
+		static void doWork() {
+			Point origin = new Point();
+			Point bottomRight = new Point(1366, 768);
+
+			double distance = origin.DistanceTo(bottomRight);
+			Console.WriteLine($"Distance is: {distance}");
+
+			Console.WriteLine($"Number of Point object: {Point.ObjectCount()}");
+		}
+	}
+}
