91da1892657a77f3577933d5764df1264dab78b1
kriosereb <kriosereb@gmail.com>
2021-04-27 17:13:42 +0300

    Add StructsAndEnums in Chapter09

---
+++ Chapter09/StructsAndEnums/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StructsAndEnums {
+	class Program {
+		static void Main(string[] args) {
+			try {
+				doWork();
+			} catch (Exception ex) {
+				Console.WriteLine(ex.Message);
+			}
+		}
+
+		static void doWork() {
+			Date defaultDate = new Date();
+			Console.WriteLine(defaultDate);
+			Date birthdayDate = new Date(1998, Month.January, 26);
+			Console.WriteLine(birthdayDate);
+
+			Date birthdayDateCopy = birthdayDate;
+			Console.WriteLine($"Value of copy is {birthdayDateCopy}");
+
+			birthdayDate.AdvanceMonth();
+			Console.WriteLine($"New value of birthdayDate is {birthdayDate}");
+			Console.WriteLine($"Value of copy is still {birthdayDateCopy}");
+		}
+	}
+}
